490871f42fd959d92a556f65c5947d2aeaa88e3f
Rasmus Talioja <30556569+kaevupoiss@users.noreply.github.com>
2021-10-21 19:18:03 +0300

    Some rework and price functionality

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+function getProduct() {
+  const queryString = window.location.search
+  let productid = new URLSearchParams(queryString).get('id')
+  let price_product = document.getElementById('price-product')
+
+  if (productid == null) {
+    price_product.style.display = 'none'
+    return
+  }
+
+  price_product.getElementsByTagName('h2')[0].textContent = 'Toode ' + productid
+  price_product.getElementsByTagName('img')[0].src = 'Kamin' + productid + '.jpg'
+}
